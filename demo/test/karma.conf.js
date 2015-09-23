e9407e2ca429befa01ec8d8800117f1bc4949877
Facundo Soria <facundo.soria@devecoop.com>
2015-09-23 16:53:47 -0300

    Add directive to demo app

--- demo/test/karma.conf.js
+++ demo/test/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/simpleImageGallery/js/fblindSimpleImageGallery.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
