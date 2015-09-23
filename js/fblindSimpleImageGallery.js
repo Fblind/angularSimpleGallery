e440e231a07f40b3b0666e5d1888c77c83518b8a
Facundo Soria <facundo.soria@devecoop.com>
2015-09-23 16:42:09 -0300

    Update basic styles

--- js/fblindSimpleImageGallery.js
+++ js/fblindSimpleImageGallery.js
@@ -2,24 +2,26 @@
 angular.module('fblindSimpleImageGallery', [])
   .directive('simpleImageGallery',
   function () {
-    var template =  '<div id="simple-gallery" class="col-sm-12 image">' +
-                      '<div class="item active">' +
-                        '<img ng-src="{{ currentImage || images[0] }}" class="img-responsive">' +
-                      '</div>' +
-                    '</div>' +
-                    '<div class="col-sm-12">' +
-                      '<div class="row">' +
-                        '<div class="col-sm-12 text-center">' +
-                          '<ul>' +
-                            '<li ng-repeat="image in images track by $index">' +
-                              '<a ng-click="activateImg($index)" href="">' +
-                                '<img ng-src="{{ image }}" class="img-responsive thumbnail-wizard">' +
-                              '</a>' +
-                            '</li>' +
-                          '</ul>' +
+    var template =  '<div id="simple-gallery">' +
+                      '<div id="simple-gallery" class="col-sm-12 image">' +
+                        '<div class="item active">' +
+                          '<img ng-src="{{ currentImage || images[0] }}" class="img-responsive">' +
                         '</div>' +
                       '</div>' +
-                    '</div>';
+                      '<div class="col-sm-12">' +
+                        '<div class="row">' +
+                          '<div class="col-sm-12" id="slider-thumbs">' +
+                            '<ul>' +
+                              '<li ng-repeat="image in images track by $index">' +
+                                '<a ng-click="activateImg($index)" href="">' +
+                                  '<img ng-src="{{ image }}" class="img-responsive simple-gallery-thumbnail">' +
+                                '</a>' +
+                              '</li>' +
+                            '</ul>' +
+                          '</div>' +
+                        '</div>' +
+                        '</div>' +
+                      '</div>';
     return {
       restrict: 'E',
       scope: {
