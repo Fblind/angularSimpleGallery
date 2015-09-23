764d63f605717aa0f44aa764cde451b84ff81060
Facundo Soria <facundo.soria@devecoop.com>
2015-09-23 15:57:37 -0300

    Add demo structure with angular-yeoman

---
+++ demo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleImageGalleryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the simpleImageGalleryApp
+ */
+angular.module('simpleImageGalleryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
