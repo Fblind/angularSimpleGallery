7cd838dab887cead8688a2d6cffb01b064762d96
Facundo Soria <facundo.soria@devecoop.com>
2015-09-23 15:14:50 -0300

    Indent template

--- js/fblindSimpleImageGallery.js
+++ js/fblindSimpleImageGallery.js
@@ -2,25 +2,24 @@
 angular.module('fblindSimpleImageGallery')
   .directive('simpleImageGallery',
   function () {
-    var template = '<div class="col-sm-12 image">' +
-      '<div class="item active">' +
-      '<img ng-src="{{ currentImage || images[0] }}" class="img-responsive">' +
-      '</div>' +
-      '</div>' +
-      '<div class="col-sm-12">' +
-      '<div class="row">' +
-      '<div class="col-sm-12" id="slider-thumbs">' +
-      '<ul>' +
-      '<li ng-repeat="image in images track by $index">' +
-      '<a ng-click="activateImg($index)" href="">' +
-      '<img ng-src="{{ image }}" class="img-responsive thumbnail-wizard">' +
-      '</a>' +
-      '</li>' +
-      '</ul>' +
-      '</div>' +
-      '</div>' +
-      '</div>' +
-      '</div>';
+    var template =  '<div class="col-sm-12 image">' +
+                      '<div class="item active">' +
+                        '<img ng-src="{{ currentImage || images[0] }}" class="img-responsive">' +
+                      '</div>' +
+                    '</div>' +
+                    '<div class="col-sm-12">' +
+                      '<div class="row">' +
+                        '<div class="col-sm-12 text-center">' +
+                          '<ul>' +
+                            '<li ng-repeat="image in images track by $index">' +
+                              '<a ng-click="activateImg($index)" href="">' +
+                                '<img ng-src="{{ image }}" class="img-responsive thumbnail-wizard">' +
+                              '</a>' +
+                            '</li>' +
+                          '</ul>' +
+                        '</div>' +
+                      '</div>' +
+                    '</div>';
     return {
       restrict: 'E',
       scope: {
